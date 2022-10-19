2ca97ccf79f6abd3a4ac5d267e4510a02b3ba92a
Joaquin <joacomda@gmail.com>
2022-10-19 19:41:30 +0200

    Queue Topic and Binding

--- n0tification/src/main/java/com/n0tification/NotificationConfig.java
+++ n0tification/src/main/java/com/n0tification/NotificationConfig.java
@@ -1,6 +1,11 @@
 package com.n0tification;
 
+import org.springframework.amqp.core.Binding;
+import org.springframework.amqp.core.BindingBuilder;
+import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.core.TopicExchange;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 
 public class NotificationConfig {
 
@@ -12,6 +17,24 @@ public class NotificationConfig {
 
     @Value("${rabbitmq.routing-key.internal.notification}")
     private String internalNotificationRoutingKey;
+
+    @Bean
+    public TopicExchange internalTopicExchange() {
+        return new TopicExchange(this.internalExchange);
+    }
+    @Bean
+    public Queue notificationQueue() {
+        return new Queue(this.notificationQueue);
+    }
+
+    @Bean
+    public Binding internalToNotificationBinding() {
+        return BindingBuilder
+                .bind(notificationQueue())
+                .to(internalTopicExchange())
+                .with(this.internalNotificationRoutingKey);
+    }
+
 
 
 
