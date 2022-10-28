cccb00e334ef5191306576c794869176cf54e4d9
Joaquin <joacomda@gmail.com>
2022-10-28 09:26:49 +0200

    Refactoring Feing Clients

--- clients/src/main/java/com/clients/n0tification/NotificationClient.java
+++ clients/src/main/java/com/clients/n0tification/NotificationClient.java
@@ -6,7 +6,10 @@ import org.springframework.cloud.openfeign.FeignClient;
 
 import org.springframework.web.bind.annotation.PostMapping;
 
-@FeignClient("notification")
+@FeignClient(
+        name = "notification",
+        url = "${clients.notification.url}"
+)
 public interface NotificationClient {
 
 
--- n0tification/src/main/java/com/n0tification/NotificationApplication.java
+++ n0tification/src/main/java/com/n0tification/NotificationApplication.java
@@ -5,10 +5,15 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.annotation.PropertySources;
 
 
 @SpringBootApplication(
         scanBasePackages = {"com.n0tification", "com.amqp"}
+)
+@PropertySources(
+        @PropertySource("classpath:clients-${spring.profiles.active}.properties")
 )
 public class NotificationApplication {
     public static void main(String[] args) {
